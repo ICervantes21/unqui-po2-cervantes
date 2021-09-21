037ae987e27aedf22b8813216bea906c10817d97
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-09-20 21:52:08 -0300

    Punto 1 (no) completo...

--- src/unq/edu/ar/po2/tp3/Counter.java
+++ src/unq/edu/ar/po2/tp3/Counter.java
@@ -1,5 +1,33 @@
 package unq.edu.ar.po2.tp3;
 
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Counter {
+	
+	private ArrayList<Integer> contador = new ArrayList<Integer>();
+	
+	public void addNumber(int número) {
+		this.contador.add(número);
+	}
+	
+	public int getEvenOcurrences() {
+		Stream<Integer> amount = contador.stream().filter(número -> número % 2 == 0);
+		return amount.toList().size();
+	}
+	
+	public int getNotEvenOcurrences() {
+		Stream<Integer> amount = contador.stream().filter(número -> número % 2 != 0);
+		return amount.toList().size();
+	}
+	
+	public int getMultiplosDe(int número) {
+		Stream<Integer> amount = contador.stream().filter(number -> this.esMultiplo(número, number) );
+		return amount.toList().size();
+	}
+	
+	public boolean esMultiplo(int número1, int número2) {
+		return Math.floorMod(número1, número2) == 0;
+	}
 
 }
---
+++ src/unq/edu/ar/po2/tp3/CounterTestCase.java
@@ -0,0 +1,64 @@
+package unq.edu.ar.po2.tp3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CounterTestCase {
+	private Counter counter;
+	/** Crea un escenario de test básico, que consiste en un contador
+	 * con 10 enteros.
+	 * 
+	 * @Throws Exception
+	 */
+	
+	@Before
+	public void setUp() throws Exception {
+		//Se crea el contador.
+		counter = new Counter();
+		
+		//Se agregan los números.
+		counter.addNumber(1);
+		counter.addNumber(3);
+		counter.addNumber(5);
+		counter.addNumber(7);
+		counter.addNumber(9);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(1);
+		counter.addNumber(4);
+	}
+	
+	/** 
+	 * Verifica la cantidad de pares.
+	 */
+	
+	@Test
+	public void testEvenNumbers() {
+		int amount = counter.getEvenOcurrences();
+		assertEquals(1, amount);
+	}
+	
+	/**
+	 * Verifica la cantidad de impares.
+	 */
+	
+	@Test
+	public void testNotEvenNumbers() {
+		int amount = counter.getNotEvenOcurrences();
+		assertEquals(9, amount);
+	}
+	
+	/**
+	 * Verifica la cantidad de múltiplos de un número.
+	 */
+	
+	@Test
+	public void testMúltiplos() {
+		int amount = counter.getMultiplosDe(7); //should be 3...
+		assertEquals(6, amount); //no anda
+	}
+
+}
