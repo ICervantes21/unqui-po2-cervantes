efa9e9ab6c84fd3865c7d30c3bb20b6c7fc597d5
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-09-27 13:59:30 -0300

    Errores menores corregidos en PersonaEncapsulada se cambió el import Date por Calendar

--- src/unq/edu/ar/po2/tp3/PersonaEncapsulada.java
+++ src/unq/edu/ar/po2/tp3/PersonaEncapsulada.java
@@ -1,24 +1,44 @@
 package unq.edu.ar.po2.tp3;
 
-import java.util.Date;
+import java.util.Calendar;
+
+/**
+ * 
+ * @author Ignacio CErvantes
+ * 
+ * TP3, ejercicio 10: encapsulamiento.
+ *
+ */
 
 public class PersonaEncapsulada {
 	
+	@SuppressWarnings("unused")
 	private String nombre;
-	private Date fechaDeNac;
+	private Calendar fechaDeNac;
 	
 	
 	
-	public PersonaEncapsulada(String nombre, Date fechaDeNac, int edad) {
+	public PersonaEncapsulada(String nombre, Calendar fechaDeNac, int edad) {
 		this.nombre = nombre;
 		this.fechaDeNac = fechaDeNac;
 		
 	}
 	
-	@SuppressWarnings("deprecation")
+	/**
+	 * 
+	 * @return La edad de la persona.
+	 */
+	
 	public int edad() {
-		return 2021 - this.fechaDeNac.getYear();
+		return 2021 - this.fechaDeNac.get(Calendar.YEAR);
 	}
+	
+	/**
+	 * 
+	 * @param persona Debe ser una PersonaEncapsulada.
+	 * @return Verdadero si la edad de la instancia receptora del mensaje es
+	 * menor a la recibida en el parámetro, falso en caso contrario.
+	 */
 	
 	public boolean menorQue(PersonaEncapsulada persona) {
 		if (this.edad() < persona.edad()) {
