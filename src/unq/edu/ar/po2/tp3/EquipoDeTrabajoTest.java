0e7c85714a2d89aac39fff97b0c017c2c684ed41
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-09-27 14:44:24 -0300

    Punto 11 completo

--- src/unq/edu/ar/po2/tp3/EquipoDeTrabajoTest.java
+++ src/unq/edu/ar/po2/tp3/EquipoDeTrabajoTest.java
@@ -9,6 +9,10 @@ import org.junit.Test;
 
 public class EquipoDeTrabajoTest {
 	
+	/**
+	 * Instancio a las personas y al equipo como estáticas para luego poder imprimir en el main.
+	 */
+	
 	static Persona persona1 = new Persona("Juan Carlos", 30);
 	static Persona persona2 = new Persona("Juan Perez", 22);
 	static Persona persona3 = new Persona("Ignacio Cervantes", 24);
@@ -16,12 +20,19 @@ public class EquipoDeTrabajoTest {
 	static Persona persona5 = new Persona("Juana Santoro", 19);
 	static EquipoDeTrabajo equipo = new EquipoDeTrabajo("Java Team", Arrays.asList(persona1, persona2, persona3, persona4, persona5));
 	
-	
+	/**
+	 * También verifico el promedio de edades mediante un test.
+	 */
 	
 	@Test
 	public void testPromedio() {
 		assertEquals(23, equipo.promedioDeEdades());
 	}
+	
+	/**
+	 * 
+	 * @param args Imprime 23.
+	 */
 	
 	public static void main(String[] args) {
 		int promedio = equipo.promedioDeEdades();
