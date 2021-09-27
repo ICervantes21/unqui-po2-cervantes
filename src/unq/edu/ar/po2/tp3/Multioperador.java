d30fabb011a9172b9bae9c060ac527dd41b246e5
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-09-27 12:25:48 -0300

    Multioperador ya puede sumar todos los números de una lista (verificado con test)

--- src/unq/edu/ar/po2/tp3/Multioperador.java
+++ src/unq/edu/ar/po2/tp3/Multioperador.java
@@ -1,5 +1,19 @@
 package unq.edu.ar.po2.tp3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Multioperador {
+	
+	private List<Integer> listaDeEnteros = new ArrayList<Integer>();
+	
+	public int sumarLosEnteros() {
+		int resultado = listaDeEnteros.stream().reduce(0, (acumulado, nuevo) -> (acumulado + nuevo));
+		return resultado;
+	}
+	
+	public void addNumber(int número) {
+		listaDeEnteros.add(número);
+	}
 
 }
---
+++ src/unq/edu/ar/po2/tp3/MultioperadorTest.java
@@ -0,0 +1,29 @@
+package unq.edu.ar.po2.tp3;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class MultioperadorTest {
+	Multioperador multioperador = new Multioperador();
+	
+	
+	@Before
+	public void setUp() throws Exception {
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+		multioperador.addNumber(5);
+	}
+	
+	@Test
+	public void testSuma() {
+		assertEquals(40, multioperador.sumarLosEnteros());
+	}
+
+}
