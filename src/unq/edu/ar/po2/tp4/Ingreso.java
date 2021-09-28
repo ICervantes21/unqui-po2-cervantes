4b06826ac935b89986b4c116a612fe3c86e6e7a7
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-09-28 14:13:29 -0300

    Falta competar punto 3

--- src/unq/edu/ar/po2/tp4/Ingreso.java
+++ src/unq/edu/ar/po2/tp4/Ingreso.java
@@ -1,5 +1,11 @@
 package unq.edu.ar.po2.tp4;
 
+import java.util.Calendar;
+
 public class Ingreso {
+	
+	private Calendar mesDePercepión;
+	private String conepto;
+	private int montoPercibido;
 
 }
--- src/unq/edu/ar/po2/tp4/Trabajador.java
+++ src/unq/edu/ar/po2/tp4/Trabajador.java
@@ -1,5 +1,11 @@
 package unq.edu.ar.po2.tp4;
 
 public class Trabajador {
+	
+	private double totalPercibido;
+	private double montoImponible;
+	private double impuestoAPagar = montoImponible * 0.2;
+	
+	
 
 }
