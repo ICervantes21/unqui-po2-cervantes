ca9b240d05f36a561180a5e5fd097e285a106839
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-10-21 20:12:53 -0300

    soja y trigo modelados

--- src/unq/edu/ar/po2/tp8/composite/cultivos/Cultivo.java
+++ src/unq/edu/ar/po2/tp8/composite/cultivos/Cultivo.java
@@ -2,5 +2,5 @@ package unq.edu.ar.po2.tp8.composite.cultivos;
 
 public interface Cultivo {
 	
-	private int getPrecio();
+	public int getPrecio();
 }
---
+++ src/unq/edu/ar/po2/tp8/composite/cultivos/Soja.java
@@ -0,0 +1,10 @@
+package unq.edu.ar.po2.tp8.composite.cultivos;
+
+public class Soja implements Cultivo{
+	
+	@Override
+	public int getPrecio() {
+		return 500;
+	}
+
+}
