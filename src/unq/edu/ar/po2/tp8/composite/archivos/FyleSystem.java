e6467969e41ce97ca066cfdbcf84793749ca138a
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-10-25 15:00:39 -0300

    comenzando el ejercicio fileSystem

---
+++ src/unq/edu/ar/po2/tp8/composite/archivos/FyleSystem.java
@@ -0,0 +1,31 @@
+package unq.edu.ar.po2.tp8.composite.archivos;
+
+public interface FyleSystem {
+	
+	/**
+	 * Retorna el total ocupado en disco del receptor, expresado
+	 * en cantidad de bytes
+	 */
+	
+	public int totalSize();
+	
+	/**
+	 * Imprime en consola el contenido indicando el nombre del elemento e identandolo
+	 * con tantos espacios como profundidad en la estructura.
+	 */
+	
+	public void printStructure();
+	
+	/**
+	 * Elemento mas nuevo.
+	 */
+	
+	public XXXXXX lastModified();
+	
+	/**
+	 * Elemento mas antiguo.
+	 */
+	
+	public XXXXX oldestElement();
+
+}
---
+++ src/unq/edu/ar/po2/tp8/composite/juego/Mapa.java
@@ -0,0 +1,5 @@
+package unq.edu.ar.po2.tp8.composite.juego;
+
+public class Mapa {
+
+}
