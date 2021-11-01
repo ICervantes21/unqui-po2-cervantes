4a4ee7292d0bbd8a22e77b45da1f59cffad1214f
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-11-01 20:42:37 -0300

    Ya se puede construir una carta

--- src/unq/edu/ar/po2/tp7/doubles/Carta.java
+++ src/unq/edu/ar/po2/tp7/doubles/Carta.java
@@ -1,8 +1,23 @@
 package unq.edu.ar.po2.tp7.doubles;
 
 public class Carta {
-	
+
 	private int número;
 	private Palo palo;
+
+	public int getNúmero() {
+		return número;
+	}
+
+	public Palo getPalo() {
+		return palo;
+	}
+
+	public Carta(int número, Palo palo) {
+		this.número = número;
+		this.palo = palo;
+	}
+	
+	
 
 }
