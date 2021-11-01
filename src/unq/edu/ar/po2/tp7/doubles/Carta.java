d68360626985dcd09db3b006d39bc315b44113a0
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-11-01 20:54:19 -0300

    La clase carta ya esta modelada.

--- src/unq/edu/ar/po2/tp7/doubles/Carta.java
+++ src/unq/edu/ar/po2/tp7/doubles/Carta.java
@@ -2,20 +2,28 @@ package unq.edu.ar.po2.tp7.doubles;
 
 public class Carta {
 
-	private int número;
+	private int valor;
 	private Palo palo;
 
-	public int getNúmero() {
-		return número;
+	public int getValor() {
+		return valor;
 	}
 
 	public Palo getPalo() {
 		return palo;
 	}
 
-	public Carta(int número, Palo palo) {
-		this.número = número;
+	public Carta(int valor, Palo palo) {
+		this.valor = valor;
 		this.palo = palo;
+	}
+	
+	public boolean es_masGrandeQue_(Carta carta1, Carta carta2) {
+		return carta1.getValor() > carta2.getValor();
+	}
+	
+	public boolean sonDelMismoPalo(Carta carta1, Carta carta2) {
+		return carta1.getPalo() == carta2.getPalo();
 	}
 	
 	
