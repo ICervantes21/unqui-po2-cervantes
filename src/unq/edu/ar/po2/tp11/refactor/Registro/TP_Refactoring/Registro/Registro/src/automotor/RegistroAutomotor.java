ba5dcaa96918025dc2d1e06f9e178e0f69c371cf
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-11-26 15:12:44 -0300

    Refactor long method realizado, pasan todos los test

--- src/unq/edu/ar/po2/tp11/refactor/Registro/TP_Refactoring/Registro/Registro/src/automotor/RegistroAutomotor.java
+++ src/unq/edu/ar/po2/tp11/refactor/Registro/TP_Refactoring/Registro/Registro/src/automotor/RegistroAutomotor.java
@@ -5,22 +5,42 @@ import java.time.LocalDate;
 /**
  * 
  * @author Ignacio Cervantes
- *
- * Possible bad smell: feature envy
  */
 public class RegistroAutomotor {
 
+	/**
+	 * Long method!!
+	 * 
+	 * @param vehiculo
+	 * @param fecha
+	 * @return
+	 */
 	public Boolean debeRealizarVtv(Vehiculo vehiculo, LocalDate fecha) {
 
-		LocalDate fechaFabricacion = vehiculo.getFechaFabricacion();
-		Boolean esVehiculoPolicial = vehiculo.esVehiculoPolicial();
-		String ciudadRadicacion = vehiculo.ciudadRadicacion();
-		
-		// vehiculos policiales no realizan vtv, ya que tienen otro tipo de control
-		// sólo realizan vtv los vehículos con más de 1 anio de antiguedad y radicados
-		// en 'Buenos Aires'
-		return (!esVehiculoPolicial && fecha.minusYears(1).isAfter(fechaFabricacion)
-				&& ciudadRadicacion.equalsIgnoreCase("Buenos Aires"));
+		/*
+		 * LocalDate fechaFabricacion = vehiculo.getFechaFabricacion(); Boolean
+		 * esVehiculoPolicial = vehiculo.esVehiculoPolicial(); String ciudadRadicacion =
+		 * vehiculo.ciudadRadicacion();
+		 */
 
+		// vehiculos policiales no realizan vtv, ya que tienen otro tipo de control
+		// sólo realizan vtv los vehículos con más de 1 anio de antiguedad y
+		// radicados
+		// en 'Buenos Aires'
+		return (this.vehiculoNoPolicial(vehiculo) && this.tieneMasDeUnA�o(vehiculo, fecha)
+				&& this.esDeBuenosAires(vehiculo));
+
+	}
+
+	public boolean vehiculoNoPolicial(Vehiculo vehiculo) {
+		return !vehiculo.esVehiculoPolicial();
+	}
+
+	public boolean tieneMasDeUnA�o(Vehiculo vehiculo, LocalDate fecha) {
+		return fecha.minusYears(1).isAfter(vehiculo.getFechaFabricacion());
+	}
+
+	public boolean esDeBuenosAires(Vehiculo vehiculo) {
+		return vehiculo.ciudadRadicacion().equalsIgnoreCase("Buenos Aires");
 	}
 }
