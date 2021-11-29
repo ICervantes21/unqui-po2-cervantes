ca17e0020233d9bd7856ad63ae0424f296f0dbab
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2021-11-29 13:26:21 -0300

    Encriptador testeado con test doubles

---
+++ src/unq/edu/ar/po2/tp10/state/encriptador/EncriptadorTest.java
@@ -0,0 +1,44 @@
+package unq.edu.ar.po2.tp10.state.encriptador;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EncriptadorTest {
+
+	EncriptadorNaive encriptador;
+	EncriptadorConVocales state;
+	String A;
+
+	@Before
+	public void setUp() throws Exception {
+		encriptador = new EncriptadorNaive(state);
+		state = mock(EncriptadorConVocales.class);
+		encriptador = new EncriptadorNaive(state);
+	}
+
+	@Test
+	public void testDoubleEncriptar() {
+		// Exercise
+		when(encriptador.encriptar(A)).thenReturn("Encriptado");
+
+		// Verify
+		assertTrue(encriptador.encriptar(A).equals("Encriptado"));
+	}
+	
+	/**
+	 * Utilizo el == para comparar strings por un error en el mensaje equals.
+	 */
+	@Test
+	public void testDesencriptarDouble() {
+		// Exercise
+				when(encriptador.desencriptar("Encriptado")).thenReturn(A);
+
+				// Verify
+				assertTrue(encriptador.desencriptar("Encriptado") == A); 
+	}
+}
